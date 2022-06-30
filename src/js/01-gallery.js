76cf1842821a698e4252163a551a4ba33a99cdd0
Irina1234567890 <irina3213@gmail.com>
2022-06-30 19:26:17 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function onCreateItem(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href='${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        data-source='${original}'
+        alt='${description}'
+        />
+    </a>
+    
+    `}).join('');
+  }
+
+const item = onCreateItem(galleryItems);
+gallery.insertAdjacentHTML('beforeend', item);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt',
+captionDelay: 250 });
+
+SimpleLightbox.addEventListener('click', onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  const instance = lightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">`)
+
+    instance.show();
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyModal);
+  document.body.classList.close(".basicLightbox--visible");
+}
+
+function onEscKeyModal(event) {
+  if (event.code === "Escape") {
+    onCloseModal();
+  }
+}
+
