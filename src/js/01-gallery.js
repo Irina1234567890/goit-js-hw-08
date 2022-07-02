087cbd31f45d97a6c62773edaddf5b2e4fa822fe
Irina1234567890 <irina3213@gmail.com>
2022-07-02 14:16:00 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,51 +1,85 @@
+// Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
 import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+const galleryWrapper = document.querySelector('.gallery');
 
-function onCreateItem(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `
-    <a class="gallery__item" href='${original}'>
-      <img
-        class="gallery__image"
-        src='${preview}'
-        data-source='${original}'
-        alt='${description}'
-        />
-    </a>
+
+const imgList = galleryItems.map(({preview,description,original})=> `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a> `).join('');
+  galleryWrapper.insertAdjacentHTML('beforeend',imgList);
+
+  galleryWrapper.addEventListener('click' , bigImgGallery);
+
+  let gallery = new SimpleLightbox('.gallery a',{
+    captionsData:`alt`,
+    captionDelay:250 
+  });
+
+  function bigImgGallery (event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+}
+
+
+
+
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+// console.log(galleryItems);
+
+// const gallery = document.querySelector('.gallery');
+
+// function onCreateItem(galleryItems) {
+//   return galleryItems.map(({ preview, original, description }) => {
+//     return `
+//     <a class="gallery__item" href='${original}'>
+//       <img
+//         class="gallery__image"
+//         src='${preview}'
+//         data-source='${original}'
+//         alt='${description}'
+//         />
+//     </a>
     
-    `}).join('');
-  }
+//     `}).join('');
+//   }
 
-const item = onCreateItem(galleryItems);
-gallery.insertAdjacentHTML('beforeend', item);
+// const item = onCreateItem(galleryItems);
+// gallery.insertAdjacentHTML('beforeend', item);
 
-const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt',
-captionDelay: 250 });
+// const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt',
+// captionDelay: 250 });
 
-SimpleLightbox.addEventListener('click', onImageClick);
+// SimpleLightbox.addEventListener('click', onImageClick);
 
-function onImageClick(evt) {
-  evt.preventDefault();
-  const instance = lightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">`)
+// function onImageClick(evt) {
+//   evt.preventDefault();
+//   const instance = lightbox.create(`
+//     <img src="${evt.target.dataset.source}" width="800" height="600">`)
 
-    instance.show();
-}
+//     instance.show();
+// }
 
-function onCloseModal() {
-  window.removeEventListener("keydown", onEscKeyModal);
-  document.body.classList.close(".basicLightbox--visible");
-}
+// function onCloseModal() {
+//   window.removeEventListener("keydown", onEscKeyModal);
+//   document.body.classList.close(".basicLightbox--visible");
+// }
 
-function onEscKeyModal(event) {
-  if (event.code === "Escape") {
-    onCloseModal();
-  }
-}
+// function onEscKeyModal(event) {
+//   if (event.code === "Escape") {
+//     onCloseModal();
+//   }
+// }
 
