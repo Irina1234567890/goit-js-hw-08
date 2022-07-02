c23f22076c987ebf4bb2f8b0aa67f04005d5b285
Irina1234567890 <irina3213@gmail.com>
2022-07-02 14:24:47 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,62 +1,108 @@
-import throttle from 'lodash.throttle';
+import throttle from "lodash.throttle";
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-};
+const formRef = document.querySelector('.feedback-form');
+const textAreaRef = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
 
-const STORAGE_KEY = 'feedback-form-state';
+formRef.addEventListener('submit', onFormSubmit);
+textAreaRef.addEventListener('input', throttle(onTextAreaInput, 500));
+formRef.addEventListener('input', throttle(savedData, 500));
+populateForm();
 
-const formData = {};
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextareaInput, 500));
-
-
-refs.form.addEventListener('input', e => {
-
-    formData[e.target.name] = e.target.value;
-});
-
-setTextareaInput();
-
-function onFormSubmit(e) {
-    e.preventDefault();
-
-    if (!e.target.message.value || !e.target.email.value) {
-        Notify.failure('Error. All fields must be filled');
-        return;
-    }
-
-    const dataSubmit = {
-    email: e.currentTarget.email.value,
-    message: e.currentTarget.message.value,
-    };
-
-    console.log('Send form');
-    console.log(dataSubmit);
-
-    e.currentTarget.reset();
+ 
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    event.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
-};
+    formData = {};
+}
 
+function savedData(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(formData));
+}
 
-function onTextareaInput(e) {
-    formData[e.target.name] = e.target.value;
-    const inputText = JSON.stringify(formData);
-    localStorage.setItem(STORAGE_KEY, inputText);
-};
+function onTextAreaInput(event) {
+   let message = event.target.value;
+    localStorage.setItem(STORAGE_KEY,message);
+}
 
-
-function setTextareaInput() {
-    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (!savedMessage) {
-        return;
+function populateForm() {
+    let savedInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    for (const key in savedInfo) {
+      if (key) {
+        formRef[key].value = savedInfo[key];
+        formData = savedInfo;
+      }
     }
-    if (savedMessage.message) {
-        refs.form.message.value = savedMessage.message;
-    }
-      if (savedMessage.email) {
-    refs.form.email.value = savedMessage.email;
   }
-};
+
+
+
+
+
+
+// import throttle from 'lodash.throttle';
+
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+// };
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// const formData = {};
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+
+// refs.form.addEventListener('input', e => {
+
+//     formData[e.target.name] = e.target.value;
+// });
+
+// setTextareaInput();
+
+// function onFormSubmit(e) {
+//     e.preventDefault();
+
+//     if (!e.target.message.value || !e.target.email.value) {
+//         Notify.failure('Error. All fields must be filled');
+//         return;
+//     }
+
+//     const dataSubmit = {
+//     email: e.currentTarget.email.value,
+//     message: e.currentTarget.message.value,
+//     };
+
+//     console.log('Send form');
+//     console.log(dataSubmit);
+
+//     e.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+// };
+
+
+// function onTextareaInput(e) {
+//     formData[e.target.name] = e.target.value;
+//     const inputText = JSON.stringify(formData);
+//     localStorage.setItem(STORAGE_KEY, inputText);
+// };
+
+
+// function setTextareaInput() {
+//     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//     if (!savedMessage) {
+//         return;
+//     }
+//     if (savedMessage.message) {
+//         refs.form.message.value = savedMessage.message;
+//     }
+//       if (savedMessage.email) {
+//     refs.form.email.value = savedMessage.email;
+//   }
+// };
 
