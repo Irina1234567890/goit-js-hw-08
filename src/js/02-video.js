f10c148235f0a4624dac4ec91b15f22295c8a430
Irina1234567890 <irina3213@gmail.com>
2022-07-03 11:50:11 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,24 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player'); 
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
-player.on('timeupdate',throttle(timeTracking,1000));
+// player.on('timeupdate',throttle(timeTracking,1000));
 
-function timeTracking (evt) {
-let currentTime = evt.seconds;
-localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
+// function timeTracking (evt) {
+// let currentTime = evt.seconds;
+// localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
     
-}
+// }
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+// player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay({ seconds }) {  
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
